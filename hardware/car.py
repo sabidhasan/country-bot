dee3d2f4afec0a0e61cb554d77ec1d31b25cbc08
Abid Hasan <abidhasan@gmail.com>
2019-06-19 22:12:40 -0600

    Add code for car class

--- hardware/car.py
+++ hardware/car.py
@@ -1,2 +1,69 @@
+import math
+import time
+
 class Car(object):
-  pass
+  WHEEL_RADIUS_CM = 1
+
+  def __init__(self, engine):
+    """
+      engine is an instance of the Engine object that controls
+      the hardware of the raspberry pi.
+    """
+    self.engine = engine
+    # Number of times a move command has been issued
+    self.moves = 0
+    # What times move commands were issued at
+    self.moves_times = []
+  
+  def get_distance_travelled(self):
+    """ Returns how much distance has been travelled in total """
+
+    circumference = 2 * math.pi * self.WHEEL_RADIUS_CM 
+    return circumference * self.engine.revolutions_per_move * self.moves
+  
+  def currently_moving(self):
+    """ Returns whether car moved in the last two seconds """
+
+    try:
+      print(self.moves_times, time.time(), abs(self.moves_times[-1] - time.time()))
+      return abs(self.moves_times[-1] - time.time()) <= self.engine.move_duration
+    except IndexError:
+      return False
+
+  def move_car(self, direction):
+    """
+      Moves the car in the direction specified by one unit, which is
+      defined as the duration in the engine. Direction is a string
+      ['straight', 'left' or 'right'].
+    """
+    directions = {
+      'straight': self.engine.go_straight,
+      'left': self.engine.go_left,
+      'right': self.engine.go_right
+    }
+    try:
+      move_success = directions[direction]()
+      self.moves_times.append(time.time())
+    except KeyError:
+      # Illegal command was issued
+      move_success = False
+    return move_success
+
+  def go_straight(self):
+    """ Moves the car forward """
+
+    return self.move_car('straight')
+
+  def go_left(self):
+    """ Moves the car forward """
+
+    return self.move_car('left')
+
+  def go_right(self):
+    """ Moves the car right """
+
+    return self.move_car('right')
+
+  def get_sensors(self):
+    """ Returns """
+    return self.engine.get_sensors()
--- test/test_Car.py
+++ test/test_Car.py
@@ -1,65 +1,51 @@
 import unittest
+from unittest.mock import Mock, MagicMock, seal
 import time
 from hardware.car import Car
 
 class CarTests(unittest.TestCase):
     def setUp(self):
-        self.car = Car()
+        engine = Mock(revolutions_per_move=2, move_duration=1)
+        engine.get_sensors =  MagicMock(return_value=[None, None])
+        engine.go_straight = MagicMock(return_value=True)
+        engine.go_left = MagicMock(return_value=True)
+        engine.go_right = MagicMock(return_value=True)
+        seal(engine.go_straight)
+        self.car = Car(engine)
 
     # Method for returning total distance travelled
     def test_distance_travelled(self):
         self.assertEqual(self.car.get_distance_travelled(), 0,
                         'initial distance is not reported as 0')
 
-    # Method for returning current speed
-    def test_get_speed(self):
-        self.assertEqual(self.car.get_speed(), 0,
-                        'initial speed is not reported as 0')
-
-    # Method for returning acceleratin
-    def test_get_acceleration(self):
-        self.assertEqual(self.car.get_acceleration(), 0,
-                        'initial acceleration is not reported as 0')
-
     # responds to input - straight
     def test_go_straight(self):
-        st = time.time()
         move = self.car.go_straight()
-        et = time.time()
         self.assertEqual(move, True, 'go straight command did not work')
-        self.assertAlmostEqual(st + 1, et, places=0, 
-                        msg='go straight command did not take ~1 sec')
 
     # responds to input - left
     def test_go_left(self):
         self.assertEqual(self.car.go_left(), True,
                         'go left command did not work')
 
-    # responds to input - left
+    # responds to input - right
     def test_go_right(self):
         self.assertEqual(self.car.go_right(), True,
                         'go right command did not work')
+    
+    # Method for checking if car is currently moving
+    def test_currently_moving(self):
+        self.assertEqual(self.car.currently_moving(), False,
+                        'car is reported to be moving, when it should not be')
+
+    # Method for checking if car is currently moving
+    def test_currently_moving(self):
+        self.car.go_straight()
+        self.assertEqual(self.car.currently_moving(), True,
+                        'car is reported to be moving, when it should not be')
+
     # List all sensors on car
     def test_list_all_sensors(self):
         len_sensors = len(self.car.get_sensors())
         self.assertEqual(len_sensors, 2,
                         'incorrect number of sensors')
-
-    # All sensors are descended from Sensor class
-    def test_all_sensors_are_sensors(self):
-        sensors = map(get_parent_class, self.car.list_sensors())
-        for item in sensors:
-            self.assertTrue(item == 'Sensors', 'type of sensor is not Sensor')
-
-    # get_image method returns instance of imagedata class
-    def test_get_image(self):
-        img = self.car.get_image()
-        self.assertEqual(type(img).__name__ == 'ImageData')
-
-    # get_sensor method returns instance of sensordata class
-    def test_get_image(self):
-        distance = self.car.get_distance()
-        self.assertEqual(type(distance).__name__ == 'SensorData')
-
-def get_parent_class(instance_of_class):
-    return type(instance_of_class).__bases__[0].__name__
