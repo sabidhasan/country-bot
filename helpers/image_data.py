43d6b70c1b0df3aad5aa1ae465eac2e96e45510d
Abid Hasan <abidhasan@gmail.com>
2019-06-24 23:28:09 -0600

    Refactor tests for Image class

--- helpers/image_data.py
+++ helpers/image_data.py
@@ -1,3 +1,19 @@
 class ImageData(object):
+  """
+    Used for storing image data from car camera.
+  """
+
   def __init__(self, raw_image):
-    pass
+    """ Raw image is numpy array from PiCamera stream capture in RGB format """
+    self.raw_image = raw_image
+    self.working_image = raw_image
+    
+    self.colored = True
+
+
+# method  desaturate
+# method  histogram
+# method  to base64
+# method  save_to_disk
+# attrib  width
+# attrib  height
