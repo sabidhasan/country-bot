ec1bcc0a36c2c152e88153fb0e9a9c0aa8deafff
Abid Hasan <abidhasan@gmail.com>
2019-06-25 19:26:59 -0600

    Fix picamera stub

--- packages/BotFakeRPi/BotFakeRPi/picamera.py
+++ packages/BotFakeRPi/BotFakeRPi/picamera.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 class PiCamera(object):
-	"""Fake class"""
+	""" Fake class for mocking PiCamera """
 	def __init__(self, resolution=(256,256)):
+		self.resolution = resolution
 		self.array = np.random.rand(*resolution)
 
 	def close(self):
@@ -12,4 +13,20 @@ class PiCamera(object):
 		pass
 
 	def capture(self, image, format=None, use_video_port=None, *args, **kwargs):
-		return str(self.array)
+		# Image is an instance of class array below
+		image.array = self.array
+
+class array(object):
+	""" Fake class for mocking picamera.array """
+	
+	def __init__(self):
+		# The array (which represents cam data), is set later from PiCamera.capture(<this object>)
+		self.array = None
+
+	@staticmethod
+	def PiRGBArray(camera):
+		"""
+			This method is called directly from outside the class to instantiate it.
+			PiCamera.array is actually another module, but here it is creatively faked.
+		"""
+		return array()
