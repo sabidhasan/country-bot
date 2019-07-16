f6d07e3b2997431513cf6f2a946c3674fec15542
Abid Hasan <abidhasan@gmail.com>
2019-07-15 19:52:14 -0600

    Binarize layers

--- learning/learn.py
+++ learning/learn.py
@@ -2,15 +2,20 @@
 import matplotlib
 matplotlib.use("Agg")
 
+# Allow importing image data class
+import sys
+sys.path.append('..')
+
 # import the necessary packages
-# from sklearn.preprocessing import LabelBinarizer
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 # from sklearn.metrics import classification_report
-# from keras.models import Sequential
+from sklearn.preprocessing import LabelBinarizer
+from keras.models import Sequential
 # from keras.layers.core import Dense
 # from keras.optimizers import SGD
 # import matplotlib.pyplot as plt
-# import numpy as np
+from hardware.image_data import ImageData
+import numpy as np
 import sqlite3
 # import random
 import pickle
@@ -40,8 +45,25 @@ for idx, row in enumerate(raw_data):
     label = 'L'
   else:
     raise ValueError("Row %s has no command" % idx) 
-  
-  # Grab image
-  image_object = io.BytesIO(row[2])
-  image_object = pickle.load(image_object)
-  print(type(image_object))
+  labels.append(label)
+
+  # Grab image as ImageData class, which is pickled in DB (depickling needs file-like obj)
+  raw_image = io.BytesIO(row[2])
+  image_data = pickle.load(raw_image)
+  # Represented as 0/1 based on liminosity
+  histogram = image_data.histogram().flatten()
+  data.append(histogram)
+
+labels = np.array(labels)
+data = np.array(data)
+
+# partition the data into training and testing sets
+trainX, testX, trainY, testY = train_test_split(data,	labels, test_size=0.25, random_state=1)
+
+# convert labels from int to vectors, using one hot encoding
+binarizer = LabelBinarizer()
+trainY = binarizer.fit_transform(trainY)
+testY = binarizer.transform(testY)
+
+print(testY)
+
